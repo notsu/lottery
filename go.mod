b2cf43eeb2b199576d00b567011fdd716550f626
Pichet Itngam <pichet.i@linecorp.com>
2019-06-07 14:31:42 +0700

    chore: initialize project

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/notsu/lottery
+
+go 1.12
+
+require (
+	github.com/graph-gophers/dataloader v5.0.0+incompatible // indirect
+	github.com/graphql-go/graphql v0.7.8
+	github.com/graphql-go/handler v0.2.3
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+)
+
+func main() {
+	// Schema
+	fields := graphql.Fields{
+		"search": &graphql.Field{
+			Name:        "Search lottery",
+			Description: "Full-text searching for lottery",
+			Type:        graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "Found: 443", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{
+		Name:   "RootQuery",
+		Fields: fields,
+	}
+
+	schemaConfig := graphql.SchemaConfig{
+		Query: graphql.NewObject(rootQuery),
+	}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	h := handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
+	})
+
+	http.Handle("/graphql", h)
+	http.ListenAndServe(":8080", nil)
+}
